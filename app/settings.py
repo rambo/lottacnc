a963f8f3bd3b27c852deadb26e4afb3abad59175
Taneli Kaivola <tanelikaivola@users.noreply.github.com>
2015-11-19 03:10:51 +0200

    Configurable HOST and PORT for the server

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,8 @@
 import serial # oh I'm gonna be so very sorry for this
 
 class Config(object):
+	HOST = "127.0.0.1"
+	PORT = 5002
 	SERIAL_PORT = "/dev/ttyUSB0"
 	SERIAL_BAUD = 2400
 	SERIAL_PARITY = serial.PARITY_EVEN
