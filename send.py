7f1dcab78a5ac3521d00874283680b3437ec3acb
Eero af Heurlin <eero.afheurlin@iki.fi>
2016-03-20 20:09:18 +0200

    error checking

--- send.py
+++ send.py
@@ -5,7 +5,13 @@ import app
 with open(sys.argv[1]) as fp:
   print "Sending %s" % sys.argv[1]
   lineno=1
+
+  if not app.serialport.getCD():
+    raise RuntimeError("Lotta is not ready to receive")
+
   for line in fp:
     print "Sendling line #%d" % lineno
     app.serialport.write(line)
+    if not app.serialport.getCD():
+      raise RuntimeError("Lotta is not ready to receive anymore")
     lineno += 1
